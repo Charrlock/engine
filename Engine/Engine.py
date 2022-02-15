984b47c574b53d98143181f43dbc8be64fc1de1b
Mikołaj <mikolaj.majkowski@gmail.com>
2022-02-16 00:38:15 +0100

    state16.02.2022

--- Engine/Engine.py
+++ Engine/Engine.py
@@ -3,7 +3,7 @@ import random
 piece_values = {"K": 60000, "k": 60000,
                 "Q": 900, "q": 900,
                 "R": 500, "r": 500,
-                "B": 315, "b": 315,
+                "B": 300, "b": 300,
                 "N": 300, "n": 300,
                 "P": 100, "p": 100}
 
@@ -34,6 +34,15 @@ white_knight_scores = [[-66, -53, -75, -75, -10, -55, -58, -70],
                        [-23, -15, 2, 0, 2, 0, -23, -20],
                        [-74, -23, -26, -24, -19, -35, -22, -69]]
 
+black_knight_scores = [[-74, -23, -26, -24, -19, -35, -22, -69],
+                       [-23, -15, 2, 0, 2, 0, -23, -20],
+                       [-18, 10, 13, 22, 18, 15, 11, -14],
+                       [-1, 5, 31, 21, 22, 35, 2, 0],
+                       [24, 24, 45, 37, 33, 41, 25, 17],
+                       [10, 67, 1, 74, 73, 27, 62, -2],
+                       [-3, -6, 100, -36, 4, 62, -4, -14],
+                       [-66, -53, -75, -75, -10, -55, -58, -70]]
+
 white_bishop_scores = [[-59, -78, -82, -76, -23, -107, -37, -50],
                        [-11, 20, 35, -42, -39, 31, 2, -22],
                        [-9, 39, -32, 41, 52, -10, 28, -14],
@@ -42,6 +51,15 @@ white_bishop_scores = [[-59, -78, -82, -76, -23, -107, -37, -50],
                        [14, 25, 24, 15, 8, 25, 20, 15],
                        [19, 20, 11, 6, 7, 6, 20, 16],
                        [-7, 2, -15, -12, -14, -15, -10, -10]]
+
+black_bishop_scores = [[-7, 2, -15, -12, -14, -15, -10, -10],
+                       [19, 20, 11, 6, 7, 6, 20, 16],
+                       [14, 25, 24, 15, 8, 25, 20, 15],
+                       [13, 10, 17, 23, 17, 16, 0, 7],
+                       [25, 17, 20, 34, 26, 25, 15, 10],
+                       [-9, 39, -32, 41, 52, -10, 28, -14],
+                       [-11, 20, 35, -42, -39, 31, 2, -22],
+                       [-59, -78, -82, -76, -23, -107, -37, -50]]
 
 white_rook_scores = [[35, 29, 33, 4, 37, 33, 56, 50],
                      [55, 29, 56, 67, 55, 62, 34, 60],
@@ -52,6 +70,15 @@ white_rook_scores = [[35, 29, 33, 4, 37, 33, 56, 50],
                      [-53, -38, -31, -26, -29, -43, -44, -53],
                      [-30, -24, -18, 5, -2, -18, -31, -32]]
 
+black_rook_scores = [[-30, -24, -18, 5, -2, -18, -31, -32],
+                     [-53, -38, -31, -26, -29, -43, -44, -53],
+                     [-42, -28, -42, -25, -25, -35, -26, -46],
+                     [-28, -35, -16, -21, -13, -29, -46, -30],
+                     [0, 5, 16, 13, 18, -4, -9, -6],
+                     [19, 35, 28, 33, 45, 27, 25, 15],
+                     [55, 29, 56, 67, 55, 62, 34, 60],
+                     [35, 29, 33, 4, 37, 33, 56, 50]]
+
 white_queen_scores = [[6, 1, -8, -104, 69, 24, 88, 26],
                       [14, 32, 60, -10, 20, 76, 57, 24],
                       [-2, 43, 32, 60, 72, 63, 43, 2],
@@ -60,6 +87,15 @@ white_queen_scores = [[6, 1, -8, -104, 69, 24, 88, 26],
                       [-30, -6, -13, -11, -16, -11, -16, -27],
                       [-36, -18, 0, -19, -15, -15, -21, -38],
                       [-39, -30, -31, -13, -31, -36, -34, -42]]
+
+black_queen_scores = [[-39, -30, -31, -13, -31, -36, -34, -42],
+                      [-36, -18, 0, -19, -15, -15, -21, -38],
+                      [-30, -6, -13, -11, -16, -11, -16, -27],
+                      [-14, -15, -2, -5, -1, -10, -20, -22],
+                      [1, -16, 22, 17, 25, 20, -13, -6],
+                      [-2, 43, 32, 60, 72, 63, 43, 2],
+                      [14, 32, 60, -10, 20, 76, 57, 24],
+                      [6, 1, -8, -104, 69, 24, 88, 26]]
 
 white_king_early_scores = [[4, 54, 47, -99, -99, 60, 83, -62],
                            [-32, 10, 55, 56, 56, 55, 10, 3],
@@ -70,6 +106,15 @@ white_king_early_scores = [[4, 54, 47, -99, -99, 60, 83, -62],
                            [-4, 3, -14, -50, -57, -18, 13, 4],
                            [17, 30, -3, -14, 6, -1, 40, 18]]
 
+black_king_early_scores = [[17, 30, -3, -14, 6, -1, 40, 18],
+                           [-4, 3, -14, -50, -57, -18, 13, 4],
+                           [-47, -42, -43, -79, -64, -32, -29, -32],
+                           [-55, -43, -52, -28, -51, -47, -8, -50],
+                           [-55, 50, 11, -4, -19, 13, 0, -49],
+                           [-62, 12, -57, 44, -67, 28, 37, -31],
+                           [-32, 10, 55, 56, 56, 55, 10, 3],
+                           [4, 54, 47, -99, -99, 60, 83, -62]]
+
 white_king_late_scores = [[-66, -53, -75, -75, -10, -55, -58, -70],
                           [-3, -6, 100, -36, 4, 62, -4, -14],
                           [10, 67, 1, 74, 73, 27, 62, -2],
@@ -79,95 +124,36 @@ white_king_late_scores = [[-66, -53, -75, -75, -10, -55, -58, -70],
                           [-23, -15, 2, 0, 2, 0, -23, -20],
                           [-74, -23, -26, -24, -19, -35, -22, -69]]
 
-DEPTH = 2
+black_king_late_scores = [[-74, -23, -26, -24, -19, -35, -22, -69],
+                       [-23, -15, 2, 0, 2, 0, -23, -20],
+                       [-18, 10, 13, 22, 18, 15, 11, -14],
+                       [-1, 5, 31, 21, 22, 35, 2, 0],
+                       [24, 24, 45, 37, 33, 41, 25, 17],
+                       [10, 67, 1, 74, 73, 27, 62, -2],
+                       [-3, -6, 100, -36, 4, 62, -4, -14],
+                       [-66, -53, -75, -75, -10, -55, -58, -70]]
+
+piece_position_scores = {"P": white_pawn_scores, "p": black_pawn_scores,
+                         "B": white_bishop_scores, "b": black_bishop_scores,
+                         "N": white_knight_scores, "n": black_knight_scores,
+                         "R": white_rook_scores, "r": black_rook_scores,
+                         "Q": white_queen_scores, "q": black_queen_scores,
+                         "K": white_king_early_scores, "k": black_king_early_scores}
+
+DEPTH = 3
 
 
 def find_random_move(valid_moves):
     return valid_moves[random.randint(0, len(valid_moves) - 1)]
 
 
-def find_best_move_worse(game_state, valid_moves):
-    turn_multiplier = 1 if game_state.whiteToMove else -1
-    opponent_min_max_score = float('inf')
-    best_player_move = None
-    for player_move in valid_moves:
-        game_state.make_move(player_move)
-        opponent_moves = game_state.get_valid_moves()
-        opponent_max_score = float('-inf')
-        for opponent_move in opponent_moves:
-            game_state.make_move(opponent_move)
-            if game_state.checkmate:
-                score = - turn_multiplier * float('inf')
-            elif game_state.stalemate:
-                score = 0
-            else:
-                score = - turn_multiplier * score_material(game_state.board)
-            if score > opponent_max_score:
-                opponent_max_score = score
-            game_state.undo_move()
-        if opponent_max_score > opponent_min_max_score:
-            opponent_min_max_score = opponent_max_score
-            best_player_move = player_move
-        game_state.undo_move()
-    return best_player_move
-
-
-def find_min_max_move(game_state, valid_moves, depth, whiteToMove):
-    global next_move
-    if depth == 0:
-        return score_material(game_state.board)
-
-    if whiteToMove:
-        max_score = float('-inf')
-        for move in valid_moves:
-            game_state.make_move(move)
-            next_moves = game_state.get_valid_moves()
-            score = find_min_max_move(game_state, next_moves, depth - 1, False)
-            if score > max_score:
-                max_score = score
-                if depth == DEPTH:
-                    next_move = move
-            game_state.undo_move()
-        return max_score
-    else:
-        min_score = float('inf')
-        for move in valid_moves:
-            game_state.make_move(move)
-            next_moves = game_state.get_valid_moves()
-            score = find_min_max_move(game_state, next_moves, depth - 1, True)
-            if score < min_score:
-                min_score = score
-                if depth == DEPTH:
-                    next_move = move
-            game_state.undo_move()
-        return min_score
-
-
-def find_best_move(game_state, valid_moves):
+def find_best_move(game_state, valid_moves, returnQueue):
     global next_move
     next_move = None
     random.shuffle(valid_moves)
     find_move_nega_max_alpha_beta(game_state, valid_moves, DEPTH, float('-inf'), float('inf'),
                                   1 if game_state.whiteToMove else -1)
-    return next_move
-
-
-def find_move_nega_max(game_state, valid_moves, depth, turn_multiplier):
-    global next_move
-    if depth == 0:
-        return turn_multiplier * score_material(game_state.board)
-
-    max_score = float('-inf')
-    for move in valid_moves:
-        game_state.make_move(move)
-        next_moves = game_state.get_valid_moves()
-        score = -find_move_nega_max(game_state, next_moves, depth - 1, -turn_multiplier)
-        if score > max_score:
-            max_score = score
-            if depth == DEPTH:
-                next_move = move
-        game_state.undo_move()
-    return max_score
+    returnQueue.put(next_move)
 
 
 def find_move_nega_max_alpha_beta(game_state, valid_moves, depth, alpha, beta, turn_multiplier):
@@ -184,6 +170,7 @@ def find_move_nega_max_alpha_beta(game_state, valid_moves, depth, alpha, beta, t
             max_score = score
             if depth == DEPTH:
                 next_move = move
+                print(move, score)
         game_state.undo_move()
         if max_score > alpha:
             alpha = max_score
@@ -202,22 +189,14 @@ def score_board(game_state):
         return 0
 
     score = 0
-    for row in game_state.board:
-        for square in row:
+    for row in range(len(game_state.board)):
+        for column in range(len(game_state.board[row])):
+            square = game_state.board[row][column]
+            if square != "_":
+                piece_positional_score = piece_position_scores[square][row][column]
+
             if square.isupper():
-                score += piece_values[square]
+                score += piece_values[square] + piece_positional_score
             elif square.islower():
-                score -= piece_values[square]
-    return score
-
-
-def score_material(board):
-    score = 0
-    for row in board:
-        for square in row:
-            if square.isupper():
-                score += piece_values[square]
-            elif square.islower():
-                score -= piece_values[square]
-
+                score -= piece_values[square] + piece_positional_score
     return score
--- Engine/Main.py
+++ Engine/Main.py
@@ -2,6 +2,7 @@ import pygame as p
 import chess
 import Valid
 import Engine
+from multiprocessing import Process, Queue
 
 p.init()
 
@@ -11,7 +12,7 @@ DIMENSIONS = 8
 SQUARE_SIZE = HEIGHT // DIMENSIONS
 MAX_FPS = 12
 IMAGE = {}
-MOVE_LOG_PANEL_WIDTH = 160
+MOVE_LOG_PANEL_WIDTH = 320
 MOVE_LOG_PANEL_HEIGHT = HEIGHT
 
 
@@ -71,17 +72,22 @@ def draw_move_log(screen, game_state, font):
     for i in range(0, len(move_log), 2):
         move_string = str(i//2 + 1) + ". " + str(move_log[i]) + " "
         if i + 1 < len(move_log):
-            move_string += str(move_log[i+1])
+            move_string += str(move_log[i+1]) + "  "
         move_texts.append(move_string)
+    moves_per_row = 4
     padding = 5
     line_spacing = 2
     textY = padding
-    for i in range(len(move_texts)):
-        text = move_texts[i]
+    for i in range(0, len(move_texts), moves_per_row):
+        text = ""
+        for j in range(moves_per_row):
+            if i + j < len(move_texts):
+                text += move_texts[i+j]
         text_object = font.render(text, True, p.Color('Black'))
         text_location = move_log_area.move(padding, textY)
         screen.blit(text_object, text_location)
         textY += text_object.get_height() + line_spacing
+
 
 def main():
     screen = p.display.set_mode((WIDTH + MOVE_LOG_PANEL_WIDTH, HEIGHT))
@@ -96,15 +102,18 @@ def main():
     running = True
     game_over = False
     player_white = True
-    player_black = True
-    move_log_font = p.font.SysFont("Helvetica", 18, False, False)
+    player_black = False
+    ai_thinking = False
+    move_finder_process = None
+    move_log_font = p.font.SysFont("Helvetica", 12, False, False)
+    move_undone = False
     while running:
         human_turn = (game_state.whiteToMove and player_white) or ( not game_state.whiteToMove and player_black)
         for e in p.event.get():
             if e.type == p.QUIT:
                 running = False
             elif e.type == p.MOUSEBUTTONDOWN:
-                if not game_over and human_turn:
+                if not game_over:
                     mouse_position = p.mouse.get_pos()
                     row = mouse_position[1]//SQUARE_SIZE
                     column = mouse_position[0]//SQUARE_SIZE
@@ -114,7 +123,7 @@ def main():
                     else:
                         square_selected = (row, column)
                         clicks.append(square_selected)
-                    if len(clicks) == 2:
+                    if len(clicks) == 2 and human_turn:
                         move = Valid.Move(clicks[0], clicks[1], game_state.board)
                         print(move.get_chess_notation())
                         for i in range(len(valid_moves)):
@@ -128,9 +137,17 @@ def main():
             elif e.type == p.KEYDOWN:
                 if e.key == p.K_z:
                     game_state.undo_move()
+                    square_selected = ()
+                    clicks = []
+                    move_made = True
+                    game_over = False
+                    if ai_thinking:
+                        move_finder_process.terminate()
+                        ai_thinking = False
+                    move_undone = True
+
                     if not player_white and not player_black:
                         game_state.undo_move()
-                    move_made = True
                 if e.key == p.K_r:
                     game_state = Valid.BoardState()
                     valid_moves = game_state.get_valid_moves()
@@ -138,17 +155,29 @@ def main():
                     clicks = []
                     move_made = False
                     game_over = False
+                    if ai_thinking:
+                        move_finder_process.terminate()
+                        ai_thinking = False
+                    move_undone = True
 
-        if not game_over and not human_turn:
-            ai_move = Engine.find_best_move(game_state, valid_moves)
-            if ai_move is None:
-                ai_move = Engine.find_random_move(valid_moves)
-            game_state.make_move(ai_move)
-            move_made = True
+        if not game_over and not human_turn and not move_undone:
+            if not ai_thinking:
+                ai_thinking = True
+                return_queue = Queue()
+                move_finder_process = Process(target=Engine.find_best_move, args=(game_state, valid_moves, return_queue))
+                move_finder_process.start()
+            if not move_finder_process.is_alive():
+                ai_move = return_queue.get()
+                if ai_move is None:
+                    ai_move = Engine.find_random_move(valid_moves)
+                game_state.make_move(ai_move)
+                move_made = True
+                ai_thinking = False
 
         if move_made:
             valid_moves = game_state.get_valid_moves()
             move_made = False
+            move_undone = False
 
         draw_board(screen, game_state.board)
         highlight_squares(screen, game_state, valid_moves, square_selected)
